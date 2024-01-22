a6e65b8be49d504925890eeeb570f530cc6ab6cb
MAHESHMATE <maheshmate2627@gmail.com>
2024-01-22 17:06:06 +0530

    add .env file in .gitignore file

--- server/app.ts
+++ server/app.ts
@@ -12,22 +12,22 @@ app.use(cookieParser());
 
 //cors (cross origin resource sharing)
 app.use(
-    cors({
-        origin: process.env.ORIGIN,
-    })
+  cors({
+    origin: process.env.ORIGIN,
+  })
 );
 
 //testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({
-        success: true,
-        message: "API is working",
-    });
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
 });
 
 //unknown route
-app.all("*", (req:Request, res:Response, next:NextFunction) => {
-    const err = new Error('Route ${req.originalUrl} not found') as any;
-    err.statusCode = 404;
-    next(err);
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error("Route ${req.originalUrl} not found") as any;
+  err.statusCode = 404;
+  next(err);
 });
